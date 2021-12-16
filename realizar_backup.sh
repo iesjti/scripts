ac51f00399a1d1658c53535e290589f5ba4c6602
iesjti <94464769+iesjti@users.noreply.github.com>
2021-12-16 11:58:07 -0300

    Add files via upload
    Script para realização de backup

---
+++ realizar_backup.sh
@@ -0,0 +1,17 @@
+############################################################################### #SCRIPT PARA REALIZAÇÃO AUTOMÁTICA DE BACKUP USANDO GOOGLE DRIVE VIA GRIVE2####
+################################################################################
+
+# Mover para pasta home que têm os arquivos
+cd /
+
+# Copiar os arquivos para a pasta que liga ao Drive
+echo 'IESJTI2021@_' | sudo -S cp -fR Servidor Backup_drive
+
+# Mover para pasta de backup
+cd 
+cd Backup_drive
+# Conecta com o Google Drive e faz upload
+echo 'IESJTI2021@_' | sudo -S grive
+
+
+
