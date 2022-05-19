f8b8f5c761fa7ca34c78aafa528d5d46868ce7cd
Setor de Tecnologia da Informação - Instituto Educacional São José <94464769+iesjti@users.noreply.github.com>
2022-05-19 12:50:48 -0300

    Update realizar_backup.sh

--- realizar_backup.sh
+++ realizar_backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-###########################SCRIPT PARA SINCRONIZAR ARQUIVOS DO SERVIDOR PRINCIPAL PARA UCS BACKUP########################################
+###########################SCRIPT FAZER O MOUNT DO SERVIDOR PRINCIPAL PARA UCS BACKUP########################################
 
 #Parte 1 - Copiar arquivos do servidor para a pasta que será montada
-echo 'Iesjti2022@%_' | sudo -S mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Biblioteca /Sincronizacao/Biblioteca
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/CGA /Sincronizacao/CGA
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/COORDENACAO /Sincronizacao/COORDENACAO
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/NAAF /Sincronizacao/NAAF
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Psicologia /Sincronizacao/Psicologia
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/PTI /Sincronizacao/PTI
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Publica /Sincronizacao/Publica
+echo 'Iesjti2022@%_' | sudo -S mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Biblioteca /Replica_main_server/Biblioteca
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/CGA /Replica_main_server/CGA
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/COORDENACAO /Replica_main_server/COORDENACAO
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/NAAF /Replica_main_server/NAAF
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Psicologia /Replica_main_server/Psicologia
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/PTI /Replica_main_server/PTI
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Publica /Replica_main_server/Publica
 
 
-#Parte 2 - Realizar a sincronização da pasta montada para o HD de backup 1 
-echo 'Iesjti2022@%_' | sudo -S rsync -auP /Sincronizacao /Servidor/
+##Parte 2 - Realizar a sincronização da pasta montada para o HD de backup 1 
+#echo 'Iesjti2022@%_' | sudo -S rsync -auP /Sincronizacao /Servidor/
 
 
