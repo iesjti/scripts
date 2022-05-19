efa289b765ea4fdb6c853d100f74b9cc94bd3636
Setor de Tecnologia da Informação - Instituto Educacional São José <94464769+iesjti@users.noreply.github.com>
2022-05-19 11:28:25 -0300

    Update realizar_backup.sh

--- realizar_backup.sh
+++ realizar_backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-###########################SCRIPT PARA REALIZAÇÃO AUTOMÁTICA DE BACKUP########################################
+###########################SCRIPT PARA SINCRONIZAR ARQUIVOS DO SERVIDOR PRINCIPAL PARA UCS BACKUP########################################
 
 #Parte 1 - Copiar arquivos do servidor para a pasta que será montada
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdminCredentials //10.0.0.220/Backup /media/backup_servidor
+echo 'Iesjti2022@%_' | sudo -S mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Biblioteca /Sincronizacao/Biblioteca
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/CGA /Sincronizacao/CGA
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/COORDENACAO /Sincronizacao/COORDENACAO
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/NAAF /Sincronizacao/NAAF
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Psicologia /Sincronizacao/Psicologia
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/PTI /Sincronizacao/PTI
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.AdministratorCredentials //10.0.0.220/Publica /Sincronizacao/Publica
 
 
 #Parte 2 - Realizar a sincronização da pasta montada para o HD de backup 1 
-echo 'IESJTI2021@_' | sudo -S rsync -auP /media/backup_servidor /media/Administrator/HD_BACKUP_1
+echo 'Iesjti2022@%_' | sudo -S rsync -auP /Sincronizacao /Servidor/
 
 
