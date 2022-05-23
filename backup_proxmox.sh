ebf509c819e399865a8c738d7fc82a7942a506b3
Setor de Tecnologia da Informação - Instituto Educacional São José <94464769+iesjti@users.noreply.github.com>
2022-05-23 12:07:25 -0300

    Update backup_proxmox.sh

--- backup_proxmox.sh
+++ backup_proxmox.sh
@@ -5,21 +5,8 @@
 #### O Proxmox está configurado para fazer o backup de todas as máquinas virtuais todo DOMINGO as 01:00h da manhã
 
 ### Os backups são salvos no disco "SVM04-BACKUP-GERAL", que possui capacidade de 1TB e é dedicado exclusivamente
-### para o backup
-
-## O primeiro passo para enviar esse backup para nuvem é fazer o Mount da pasta "Backup_Servers"
-## presente no Google Drive no HD de backup, mais precisamente na pasta "backup_drive". O comando é:
+### para o backup. Os arquivos de imagem ficam salvos na pasta DUMP.
+### O comando RCLONE é utilizado para copiar do servidor local para o google drive
 
 cd /mnt/pve/SVM04-BACKUP-GERAL/
-mkdir backup_drive
-rclone mount googleDrive:Backup_Servers /mnt/pve/SVM04-BACKUP-GERAL/backup_drive --daemon --allow-non-empty
-
-## Em seguida, é necessário sincronizar o arquivo de imagem gerado pelo Proxmox dentro da pasta dump
-## na pasta backup_drive. O comando é:
-
-rsync -auP /mnt/pve/SVM04-BACKUP-GERAL/dump/ /mnt/pve/SVM04-BACKUP-GERAL/backup_drive
-fusermount -u /mnt/pve/SVM04-BACKUP-GERAL/backup_drive
-cd
-rm -r mnt/pve/SVM04-BACKUP-GERAL/backup_drive
-
-## Cada arquivo colocado na pasta backup_drive é automaticamente sincronizado com o Google Drive
+rclone copy --progress /mnt/pve/SVM04-BACKUP-GERAL/backup_drive/dump googleDrive:Backup_Servers
