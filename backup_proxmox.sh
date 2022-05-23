16d355ad6a9ffcb9aa450a304710c8dd1020d9d6
Gabryel Figueiredo Soares <62814507+gabryelfig@users.noreply.github.com>
2022-05-23 13:45:20 -0300

    Update backup_proxmox.sh

--- backup_proxmox.sh
+++ backup_proxmox.sh
@@ -6,7 +6,7 @@
 
 ### Os backups são salvos no disco "SVM04-BACKUP-GERAL", que possui capacidade de 1TB e é dedicado exclusivamente
 ### para o backup. Os arquivos de imagem ficam salvos na pasta DUMP.
-### O comando RCLONE é utilizado para copiar do servidor local para o google drive
+### O comando RCLONE é utilizado para copiar do servidor local para o google drive.
 
 cd /mnt/pve/SVM04-BACKUP-GERAL/
 rclone copy --progress /mnt/pve/SVM04-BACKUP-GERAL/backup_drive/dump googleDrive:Backup_Servers
