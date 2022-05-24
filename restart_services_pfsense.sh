c1ecd9bc87a139563370afcce8b249f202d7cf56
Gabryel Figueiredo Soares <62814507+gabryelfig@users.noreply.github.com>
2022-05-24 07:54:32 -0300

    Create restart_services_pfsense.sh

---
+++ restart_services_pfsense.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Restart Interface LAN
+ifconfig re1 down
+ifconfig re1 up
+
+# Restart DHCP
+pfSsh.php playback restartdhcpd
+
+# Restart DNS
+pfSsh.php playback svc restart unbound
