e149477273aa4772796646384ae44f988188cf1d
Gabryel Figueiredo Soares <62814507+gabryelfig@users.noreply.github.com>
2022-05-24 15:23:26 -0300

    Update sincronizacao_para_backup.sh

--- sincronizacao_para_backup.sh
+++ sincronizacao_para_backup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-### Enviar arquivos do Servidor UCS principal para o Servidor UCS Backup via SCP
+### Enviar arquivos do Servidor UCS principal para o Servidor UCS Backup via RSYNC com SSH
 cd
 cd /
 ssh Administrator@10.0.0.220 rsync -auP /Servidor/PTI Administrator@10.0.0.21:/home/Administrator/Backup_Server
