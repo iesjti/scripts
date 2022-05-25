c903bc783eb4eedf48b14c1b71e55c00c45f5fff
Gabryel Figueiredo Soares <62814507+gabryelfig@users.noreply.github.com>
2022-05-25 10:07:14 -0300

    Update restart_services_pfsense.sh

--- restart_services_pfsense.sh
+++ restart_services_pfsense.sh
@@ -6,6 +6,7 @@ ifconfig re1 up
 
 # Restart DHCP
 pfSsh.php playback restartdhcpd
-
+pfSsh.php playback restartdhcpd
 # Restart DNS
+pfSsh.php playback svc restart unbound
 pfSsh.php playback svc restart unbound
